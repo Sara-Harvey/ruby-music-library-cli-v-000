cec8c75773b21dd44f8cab8399096e6deda45e3d
Sara Harvey <saraharveynyc@gmail.com>
2019-03-19 19:15:17 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,9 @@ require 'bundler'
 Bundler.require
 
 require_all 'lib'
+
+require_relative '../lib/concerns/findable'
+
+require_relative '../lib/artist.rb'
+require_relative '../lib/song.rb'
+require_relative '../lib/genre.rb'
